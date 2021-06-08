720ff95f93ec272765c1191232f1bf8f15bc5411
Michael Wehrley <michaelwehrley@users.noreply.github.com>
2021-06-08 09:32:18 -0500

    make bowling directory

---
+++ bowl/frames.js
@@ -0,0 +1,63 @@
+(function (BOWL) {
+  "use strict";
+  BOWL.frames = [
+`<pre>
+      _....._
+    .;;'      '-.
+  .;;: _         '.
+ /;;:'(_)          \\
+|;;:'_     _        |
+|;;:(_)   (_)       |
+|;;::.              |
+ \\;;::.            /
+  ';;::.         .'
+    '-;;:..  _.-'
+        '''''
+</pre>`,
+`<pre>
+        _....._
+      .;;'      '-.
+    .;;: _         '.
+   /;;:'(_)          \\
+  |;;:'_     _        |
+  |;;:(_)   (_)       |
+  |;;::.              |
+   \\;;::.            /
+    ';;::.         .'
+      '-;;:..  _.-'
+          '''''
+</pre>`,
+`<pre>
+                 ,ad88ba,
+               ,dP'    'Yb,
+               8"        "8
+               8          8
+               8          8
+               Y,        ,P
+               'b        d'
+                8        8
+                8        8
+                8aaaaaaaa8
+               ,8aaaaaaaa8,
+              ,dP        Yb,
+             ,8P          Y8,
+            ,dP            Yb,
+           ,8P              Y8,
+          ,dP                Yb,
+         ,8P                  Y8,
+         dP                    Yb
+         8I                    I8,
+        I8'                    '8I
+        I8                      8I
+        I8                      8I
+        '8                      8'
+         Yb                    dP
+         '8b                  d8'
+          '8baaaaaaaaaaaaaaaad8'
+           '8baaaaaaaaaaaaaad8'
+            '8b,          ,d8'
+             '8b,        ,d8'
+              'Y8baaaaaad8P'
+      </pre>`
+  ];
+}(window.BOWL));
