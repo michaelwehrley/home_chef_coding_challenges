164b3f9afc15b9fed860d19519a0759db4e562d9
Michael Wehrley <michaelwehrley@users.noreply.github.com>
2021-06-08 20:59:26 -0500

    proof of concept

--- bowl/frames.js
+++ bowl/frames.js
@@ -2,62 +2,71 @@
   "use strict";
   BOWL.frames = [
 `<pre>
-      _....._
-    .;;'      '-.
-  .;;: _         '.
- /;;:'(_)          \\
-|;;:'_     _        |
-|;;:(_)   (_)       |
-|;;::.              |
- \\;;::.            /
-  ';;::.         .'
-    '-;;:..  _.-'
-        '''''
+                                                                                                                                  ,ad88ba,                     ,ad88ba,
+                                                                                                                                ,dP'    'Yb,                 ,dP'    'Yb,
+                                                                                                                                8"        "8                 8"        "8
+                                                                                                                                8          8                 8          8
+                                                                                                                                8          8                 8          8
+                                                                                                                                Y,        ,P                 Y,        ,P
+                                                                                                                                'b        d'                 'b        d'
+                                                                                                                                 8        8                   8        8
+                                                                                                                                 8        8                   8        8
+                                                                                                                                 8aaaaaaaa8                   8aaaaaaaa8
+                                                                                                                                ,8aaaaaaaa8,                 ,8aaaaaaaa8,
+                                                                                                                               ,dP        Yb,               ,dP        Yb,
+                                                                                                                              ,8P          Y8,             ,8P          Y8,
+                                                                                                                             ,dP            Yb,           ,dP            Yb,
+                                                                                                                            ,8P              Y8,         ,8P              Y8,
+                                                                                                                           ,dP                Yb,       ,dP                Yb,
+                                                                                                                          ,8P                  Y8,     ,8P                  Y8,
+                                                                                                                          dP                    Yb     dP                    Yb
+                                                                                                                          8I                    I8,    8I                    I8,
+           _....._                                                                                                       I8'                    '8I   I8'                    '8I
+        .;;'      '-.                                                                                                    I8                      8I   I8                      8I
+      .;;: _         '.                                                                                                  I8                      8I   I8                      8I
+     /;;:'(_)          \\                                                                                                '8                      8'   '8                      8'
+    |;;:'_     _        |                                                                                                 Yb                    dP     Yb                    dP
+    |;;:(_)   (_)       |                                                                                                 '8b                  d8'     '8b                  d8'
+    |;;::.              |                                                                                                  '8baaaaaaaaaaaaaaaad8'       '8baaaaaaaaaaaaaaaad8'
+     \\;;::.            /                                                                                                   '8baaaaaaaaaaaaaad8'         '8baaaaaaaaaaaaaad8'
+      ';;::.         .'                                                                                                      '8b,          ,d8'           '8b,          ,d8'
+        '-;;:..  _.-'                                                                                                         '8b,        ,d8'             '8b,        ,d8'
+  ----------'''''--------------------------------------------------------------------------------------------------------------'Y8baaaaaad8P'---------------'Y8baaaaaad8P'---------
 </pre>`,
 `<pre>
-        _....._
-      .;;'      '-.
-    .;;: _         '.
-   /;;:'(_)          \\
-  |;;:'_     _        |
-  |;;:(_)   (_)       |
-  |;;::.              |
-   \\;;::.            /
-    ';;::.         .'
-      '-;;:..  _.-'
-          '''''
 </pre>`,
 `<pre>
-                 ,ad88ba,
-               ,dP'    'Yb,
-               8"        "8
-               8          8
-               8          8
-               Y,        ,P
-               'b        d'
-                8        8
-                8        8
-                8aaaaaaaa8
-               ,8aaaaaaaa8,
-              ,dP        Yb,
-             ,8P          Y8,
-            ,dP            Yb,
-           ,8P              Y8,
-          ,dP                Yb,
-         ,8P                  Y8,
-         dP                    Yb
-         8I                    I8,
-        I8'                    '8I
-        I8                      8I
-        I8                      8I
-        '8                      8'
-         Yb                    dP
-         '8b                  d8'
-          '8baaaaaaaaaaaaaaaad8'
-           '8baaaaaaaaaaaaaad8'
-            '8b,          ,d8'
-             '8b,        ,d8'
-              'Y8baaaaaad8P'
+                                                                                                                                  ,ad88ba,                     ,ad88ba,
+                                                                                                                                ,dP'    'Yb,                 ,dP'    'Yb,
+                                                                                                                                8"        "8                 8"        "8
+                                                                                                                                8          8                 8          8
+                                                                                                                                8          8                 8          8
+                                                                                                                                Y,        ,P                 Y,        ,P
+                                                                                                                                'b        d'                 'b        d'
+                                                                                                                                 8        8                   8        8
+                                                                                                                                 8        8                   8        8
+                                                                                                                                 8aaaaaaaa8                   8aaaaaaaa8
+                                                                                                                                ,8aaaaaaaa8,                 ,8aaaaaaaa8,
+                                                                                                                               ,dP        Yb,               ,dP        Yb,
+                                                                                                                              ,8P          Y8,             ,8P          Y8,
+                                                                                                                             ,dP            Yb,           ,dP            Yb,
+                                                                                                                            ,8P              Y8,         ,8P              Y8,
+                                                                                                                           ,dP                Yb,       ,dP                Yb,
+                                                                                                                          ,8P                  Y8,     ,8P                  Y8,
+                                                                                                                          dP                    Yb     dP                    Yb
+                                                                                                                          8I                    I8,    8I                    I8,
+                                    _....._                                                                              I8'                    '8I   I8'                    '8I
+                                 .;;'      '-.                                                                           I8                      8I   I8                      8I
+                               .;;: _         '.                                                                         I8                      8I   I8                      8I
+                              /;;:'(_)          \\                                                                       '8                      8'   '8                      8'
+                             |;;:'_     _        |                                                                        Yb                    dP     Yb                    dP
+                             |;;:(_)   (_)       |                                                                        '8b                  d8'     '8b                  d8'
+                             |;;::.              |                                                                         '8baaaaaaaaaaaaaaaad8'       '8baaaaaaaaaaaaaaaad8'
+                              \\;;::.            /                                                                          '8baaaaaaaaaaaaaad8'         '8baaaaaaaaaaaaaad8'
+                               ';;::.         .'                                                                             '8b,          ,d8'           '8b,          ,d8'
+                                 '-;;:..  _.-'                                                                                '8b,        ,d8'             '8b,        ,d8'
+  -----------------------------------'''''-------------------------------------------------------------------------------------'Y8baaaaaad8P'---------------'Y8baaaaaad8P'---------
+
       </pre>`
   ];
 }(window.BOWL));
